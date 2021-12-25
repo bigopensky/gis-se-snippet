f7f3d1edca627a60fb1ab2a41261d6c9c2e6d858
Alexander Weidauer <alex.weidauer@ifgdv.de>
2021-12-25 03:47:34 +0100

    Update README.md

--- geo-json-perl/README.md
+++ geo-json-perl/README.md
@@ -1,3 +1,58 @@
+# GIS Stack Exchange Question
+
+Question by [Joseph](https://gis.stackexchange.com/q/171279/26213).
+
+I am working with GeoJSON files and need to store them in PostgreSQL using perl.  I can do this in 2 steps:
+
+```perl
+    open (JSON,">$path/tmp/current.json");
+    print JSON $fullJSON;
+    close JSON;
+```
+followed by:
+
+```perl
+    `/usr/bin/ogr2ogr -f "PostgreSQL" PG:"dbname=database user=datauser" "$path/tmp/current.json" -nln datatable -append`;
+     unlink ("$path/tmp/current.json");
+```
+
+But I was hoping for a pure perl solution, without using the ogr2ogr executable.  I was reading about Geo::GDAL, but not sure if this is the right tool.
+
+I was experimenting a bit with some code I found:
+
+```perl
+    my $sth = $dbh->prepare("INSERT INTO datatable (geom) VALUES (GeomFromEWKT('SRID=?;POINT(? ? ?)'));")
+```
+
+But couldn't quite get the syntax right.  The whole JSON string is:  `$fullJSON`
+
+If anyone has any info or code to perform this task, I'd love to see it.  It seems pretty straight forward.
+
+
+# Answer 
+
+For me very interesting question, because I like perl and with the new
+swig based Geo::OGR, Geo::GDAL bindings created with GDAL 2.0.2 (good
+stuff since version 1.11) and maintained by Ari Jolma it seems to be
+perfect to handle geo stuff in perl too.
+
+In short: You should use the professional ogr2ogr tool and the
+following script is more a how to use GDAL with perl, how to read the
+layer infos and how to itereate over the features and create some SQL
+statements. I think perl API is very similar to the python API. I also
+avoid to handle the DBI stuff and generate a classical INSERT
+statement instead.
+
+To get an idea how to use GDAL in perl you could use 80% of the python
+cookbook (language cross over) and the perl class reference for the
+rest.
+
+I downlaod an example cities.geojson, thanks to [Michael
+Mahemoff](https://raw.githubusercontent.com/mahemoff/geodata/master/cities.geojson). All
+the settings in the perl file are static params (`$file`, `$layer`,
+`$epsg`, field keys) and matching the content of this file. I want
+only to get an idea how it works and feels.
+
 # Query GeoJSON with GDAL and Perl
 
 Small howto use the GDAL to read GeoJSON via
@@ -8,7 +63,7 @@ Jolma.
 
 The underlaying data set is in the file <cities.geojson>
 
-```
+```json
 > head -20 cities.geojson
 {
 	"type": "FeatureCollection",
@@ -33,10 +88,11 @@ The underlaying data set is in the file <cities.geojson>
 ...
 ```
 
-The tool <test-json.pl> reads the data structure of <cities.geojson>
-and generates postgresql insert statements as an app example.
+The tool [test-json.pl](./test-json.pl)> reads the data structure of
+file [cities.geojson](cities.geojson]) and generates postgresql insert
+statements as an app example.
 
-```
+```bash
 ./test-json.pl | head -20
 layer: 1 name:OGRGeoJSON features: 661 type: Point
   spatial-reference: +proj=longlat +datum=WGS84 +no_defs 
@@ -61,9 +117,10 @@ INSERT INTO table (city, geom) VALUES
 
 ## License
 
-COPYRIGHT AND LICENCE
+COPYRIGHT AND LICENCES
 
 Copyright (C) 2006 by Ari Jolma and GDAL Swig developers.
+Copyright (C) 2016 by ALexander Weidauer <alex.weidauer@huckfinn.de>
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.5 or,
--- geo-json-perl/test-json.pl
+++ geo-json-perl/test-json.pl
@@ -1,18 +1,23 @@
 #!/usr/bin/perl -w
-# -----------------------------------------------
-# Gis stack exchange test case
-# -----------------------------------------------
+# -------------------------------------------------------------------------
+# Gis stack exchange test case https://gis.stackexchange.com/q/171279/26213
+# (C) 2016 Alexander Weidauer <alex.weidauer@huckfinn.de>
+# -------------------------------------------------------------------------
 use strict;
-use Geo::OGR;
+use warnings;
+
+use Geo::GDAL;
+use Data::Dumper;
+
 # https://raw.githubusercontent.com/mahemoff/geodata/master/cities.geojson
 # Set the driver name
-my $dname  = 'GeoJSON';
+my $drvName  = 'GeoJSON';
 
 # Set the json file
 my $file   = 'cities.geojson';
 
 # Set the layer name
-my $lname = 'OGRGeoJSON';
+my $lyrName = 'OGRGeoJSON';
 
 # Set the city column name
 my $key_city = 'city';
@@ -26,34 +31,40 @@ my $table = 'table';
 # Set EPSG
 my $epsg = '4326';
 
-# Get the driver
-my $driver    = Geo::OGR::GetDriverByName($dname)
-   or die "Unkown driver $dname \n";
+# print join("\n",Geo::OGR::GetDriverNames),"\n";
+
+# Get the OGR driver
+my $driver   = Geo::OGR::Driver($drvName)
+   or die "Unknown driver $drvName \n";
 
 # Open the file 
-my $dsource   = $driver->Open($file)
-   or die "Unkown file $file \n";
+my $dataSource   = $driver->Open($file)
+   or die "Unknown file $file \n";
 
 # List the layers to investigate the stuff
 # to get all the settings above key_..., epsg
-&listLayers($dsource);
+&listLayers($dataSource);
 
 # Open the layer
-my $layer = $dsource->GetLayerByName($lname)
-    or die "Unkown layer $lname \n";
+my $layer = $dataSource->GetLayerByName($lyrName)
+    or die "Unknown layer $lyrName \n";
 
 # Iterate over the features
 while (my $data = $layer->GetNextFeature) {
     my $geom = $data->Geometry->AsText;
     my $city = $data->{$key_city};
-    $city = &escapeSqlStr($city);
+    $city    = &escapeSqlStr($city);
     print "INSERT INTO $table ($key_city, $key_geom) VALUES\n".
-        "($city, ST_GeomFromEWKT('SRID=$epsg;$geom');\n\n";
+          "($city, ST_GeomFromEWKT('SRID=$epsg;$geom');\n\n";
 }
 
+# =========================================
+# Service routines 
+# =========================================
+
 # -----------------------------------------
-# a minimal NULL and escaping routine
-# better done with DBI
+# A minimal NULL and escaping routine
+# Q&D ..better done with DBI
 sub escapeSqlStr() {
     my $str = shift;
     return 'NULL' if ! $str;
@@ -62,24 +73,26 @@ sub escapeSqlStr() {
 }
 
 # -----------------------------------------
-# GetNextFeature the layers and attributes
-# @param is a dataSource
+# List features within the layers
 sub listLayers() {
     my $dataSource = shift;
     # Iterate over the layers
     for my $lix (0..$dataSource->GetLayerCount-1) {
         my $layer = $dataSource->GetLayerByIndex($lix);
         my $srs   = $layer->GetSpatialRef;
+        $srs = $srs ? $srs->Export('Proj4') : 'UNKNOWN';
         my $gtype = $layer->GeometryType;
         my $numFt = $layer->GetFeatureCount;
+
+        
         # Layer statistics
         print "layer: ",$lix+1,
             " name:", $layer->GetName,
             " features: ", $numFt,
             " type: ", $gtype,
-            "\n  spatial-reference: ", $srs->Export('Proj4'),"\n";
+            "\n  spatial-reference: $srs\n";
 
-        # List attrbutes
+        # List attributes
         my $layerDefn = $layer->GetLayerDefn();
         for my $fix (0..$layerDefn->GetFieldCount-1) {
             my $fieldDefn = $layerDefn->GetFieldDefn($fix);
@@ -91,6 +104,7 @@ sub listLayers() {
     }
 }
 
+# EOF
 
 
 
